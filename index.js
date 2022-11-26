7c7057bac97fb57271d4f3a9b148f536f9f061d1
Mark Kantet Sangiriaki <mark.sangiriaki@student.moringaschool.com>
2022-11-26 09:03:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']) {
+    const modDrivers = drivers.slice(0, 2);
+    return modDrivers;
+}    
+const returnLastTwoDrivers = function (drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']) {
+    const modDrivers = drivers.slice(2, 4);
+    return modDrivers;
+}    
+const selectingDrivers = [
+    returnFirstTwoDrivers,  
+    returnLastTwoDrivers
+]
+function createFareMultiplier(num){
+    return function (num2) {
+        return num * num2;
+      };
+}
+const fareDoubler = function createFareMultiplier(num){
+    return num *2;
+}
+const fareTripler = function createFareMultiplier(num){
+    return num *3;
+}
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers();
+}
+    
